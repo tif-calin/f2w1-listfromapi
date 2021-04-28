31a2ce4345192229bb463131314ecaf58092bc29
tif-calin <76538249+tif-calin@users.noreply.github.com>
2021-04-28 16:36:20 -0700

    Initial commit

---
+++ src/app/App.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import './App.css';
+import React from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+  
+        My React App...
+        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
+      
+      </div>
+    );
+  }
+
+}
+
+export default App;
