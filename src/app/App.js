8bd868862afafef83fe90e7b75d1257393f79957
tif-calin <culitif@tuta.io>
2021-04-29 10:06:52 -0700

    add fetchPokedex on mount

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,35 @@
 import { Component } from 'react';
 import './App.css';
 import React from 'react';
+import request from 'superagent';
 
 import Header from './Header';
 import Footer from './Footer';
 import PokeList from './PokeList';
 
+const POKEMON_API = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
+
 class App extends Component {
   state = {
     pokedex: []
+  }
+
+  componentDidMount() {
+    this.fetchPokedex();
+  }
+
+  async fetchPokedex() {
+    try {
+      const response = await request.get(POKEMON_API);
+
+      this.setState(response.results);
+    } catch (err) {
+      console.log(err);
+    }
+    finally {
+      this.setState([]);
+    }
+
   }
 
   render() {
