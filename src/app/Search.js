47e750b51dfec10c06beaf3e590b265c1d3f20f3
tif-calin <culitif@tuta.io>
2021-04-29 23:05:31 -0700

    fetch list of types, shapes, eggGroups on load

--- src/app/Search.js
+++ src/app/Search.js
@@ -1,25 +1,75 @@
 import { Component } from 'react';
 import './Search.css';
+import request from 'superagent';
+
+const [TYPES_API, SHAPES_API, EGGGROUPS_API] = [
+  'https://pokedex-alchemy.herokuapp.com/api/pokedex/types', 
+  'https://pokedex-alchemy.herokuapp.com/api/pokedex/shapes', 
+  'https://pokedex-alchemy.herokuapp.com/api/pokedex/eggGroups'
+];
 
 export default class Search extends Component {
 
-  handleSubmit = e => {
-    e.preventDefault();
+  state = {
+    allTypes: [],
+    allShapes: [],
+    allEggGroups: [],
+    perPage: 50,
+    page: 1,
+    search: '',
+    searchField: 'pokemon',
+    sort: 'species_id',
+    reverse: false
   }
 
+  async fetchFilters() {
+    try {
+      const allTypes = (await request(TYPES_API)).body.map(obj => obj.type).sort();
+      const allShapes = (await request(SHAPES_API)).body.map(obj => obj.shape).sort();
+      const allEggGroups = (await request(EGGGROUPS_API)).body.map(obj => obj.eggGroup).sort();
+
+      this.setState({ allTypes: allTypes, allShapes: allShapes, allEggGroups: allEggGroups });
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  async componentDidMount() {
+    this.fetchFilters();
+  }
+
+  // handlers
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSearch(this.state);
+  }
+
+  handleSearchChange = e => {
+    this.setState({ search: e.target.value });
+  }
+
+  handleSearchFieldChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  // render Search component
   render() {
     return (
-      <form className="Search wrapper-h">
-        <label for="search">search</label>
-        <input className="search" name="search" placeholder="search"/>
+      <form className="Search wrapper-h" onSubmit={this.handleSubmit}>
+        {/*search*/}
+        <label htmlFor="search">search</label>
+        <input className="search" name="search" placeholder="search" onChange={this.handleSearchChange}/>
         <select className="searchField" name="searchField">
           <option value="pokemon">name</option>
         </select>
 
-        <label for="filters">filter</label>
+        {/*filters*/}
+        <label htmlFor="filters">filter</label>
         <button className="filters">⬇️</button>
 
-        <label for="sort">sort by</label>
+        {/*sort*/}
+        <label htmlFor="sort">sort by</label>
         <select className="sort" name="sort">
           <option value="species_id">pokedex #</option>
           <option value="pokemon">name</option>
@@ -33,14 +83,16 @@ export default class Search extends Component {
         </select>
         <button className="reverse" name="reverse">🔃</button>
 
-        <label for="page-nav">page nav</label>
+        {/*pages*/}
+        <label htmlFor="page-nav">page nav</label>
         <fieldset className="page-nav" name="page-nav">
           <button className="page-button">&#9664;</button>
           <input className="page-number" min="0" type="number"/>
           <button className="page-button">&#9654;</button>
         </fieldset>
 
-        <label for="per-page">per page</label>
+        {/*results per page*/}
+        <label htmlFor="per-page">per page</label>
         <select className="per-page" name="per-page">
           <option value="20">20</option>
           <option value="50">50</option>
